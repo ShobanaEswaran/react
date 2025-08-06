405ee713706a6c65326c5028bf66817f6fad87c4
ShobanaEswaran <eswaranshobana@gmail.com>
2025-08-06 10:26:31 +0530

    lesson-1

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement('h1', {id:"heading"}, "I'm a h1 tag");
+
+const parent = React.createElement('div', {id: 'parent'}, [heading, React.createElement('h2', {id:"heading2"}, "I'm a h2 tag")])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent)
