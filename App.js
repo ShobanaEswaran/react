b442f1c9dbc11a833e8b2b10da740f4f6d5d6289
ShobanaEswaran <eswaranshobana@gmail.com>
2025-08-25 15:03:07 +0530

    added dummy components

--- App.js
+++ App.js
@@ -1,17 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./App.css";
+import Logo from "./logo.png";
 
-const heading = <h1>React element heading</h1>;
-
-const Title = () => <h1>React component heading</h1>;
-
-const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    <h1>React composition heading</h1>
+const Header = () => (
+  <div id="header">
+    <div>
+      <img
+        src={"https://github.com/ShobanaEswaran/react/logo.png"}
+        alt="Logo"
+      />
+    </div>
+    <div className="menus">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+    </div>
   </div>
 );
 
+const Footer = () => (
+  <div id="footer">
+    <h4>© 2025 HappyFoodCourt Limited</h4>
+  </div>
+);
+
+const RestaurantCard = (props) => {
+  return <div className="restaurant-card"></div>;
+};
+
+const Main = () => (
+  <div id="main">
+    <div>Search</div>
+    <div className="restaurant-cards-container">
+      <RestaurantCard />
+    </div>
+  </div>
+);
+
+const AppLayout = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
